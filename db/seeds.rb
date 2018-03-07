211c7ea49c0f69b1a9914755b9ba05d5b80e79a3
Richard Song <richardsong18@gmail.com>
2018-03-06 19:06:05 -0600

    created two example products in the seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ product = Product.new(name:"iPhone", price:700, image_url:"https://goo.gl/images/ZXysWy", description:"Apple phone")
+ product.save
+ product = Product.new(name:"Galaxy S8", price:750, image_url:"https://goo.gl/images/5fZZP1", description:"Samsung phone")
+ product.save
+
+ #product = Product.create(...) .create does .new and then .save in one method 
