abe80c9ca5f1e1fe25001f4350703978b935bc12
Richard Song <richardsong18@gmail.com>
2018-03-06 19:50:21 -0600

    added two routes to individually show the first and second product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,15 @@ class ProductsController < ApplicationController
     products = Product.all 
     render json: {products: products}
   end 
+
+  def first_product
+    product = Product.find_by(id: 1)
+    render json: {product_1: product}
+  end 
+
+  def second_product
+    product = Product.find_by(id: 2)
+    render json: {product_2: product}
+  end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get "/products" => "products#show_products" 
-
+  get "/first_product" => "products#first_product"
+  get "/second_product" => "products#second_product"
 end
