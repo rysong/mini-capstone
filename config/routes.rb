18f99a17d6a5d235eee01e60295672a4dd823cd1
Richard Song <richardsong18@gmail.com>
2018-03-07 21:25:45 -0600

    created a single route that can display ANY single product using a url segment parameter

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
     get "/second_product" => "products#second_product"
 
     get "/any_product" => "products#any_product"
+    get "/any_product/:id" => "products#any_product"
   end 
 end
