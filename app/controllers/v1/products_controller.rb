6e7b4154f4b4da30aedf4e90993ab37caa554f5d
Richard Song <richardsong18@gmail.com>
2018-03-08 18:47:40 -0600

    update controller action for displaying one product

--- app/controllers/v1/products_controller.rb
+++ app/controllers/v1/products_controller.rb
@@ -1,23 +1,14 @@
 class V1::ProductsController < ApplicationController
+  
   def index 
     products = Product.all 
     render json: products.as_json #as_json is assumed if not typed in
   end 
 
-  def first_product
-    product = Product.find_by(id: 1)
+  def show
+    product_id = params["id"]
+    product = Product.find_by(id: product_id)
     render json: product.as_json 
   end 
-
-  def second_product
-    product = Product.find_by(id: 2)
-    render json: {product_2: product}
-  end 
-
-  def any_product 
-    id = params["id"]
-    product = Product.find_by(id: id) 
-    render json: {product: product} 
-  end 
-
+ 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   namespace :v1 do 
     get "/products" => "products#index" 
-    
-    get "/first_product" => "products#first_product"
-    get "/second_product" => "products#second_product"
+    get "/products/:id" => "products#show"
 
     get "/any_product" => "products#any_product"
     get "/any_product/:id" => "products#any_product"
