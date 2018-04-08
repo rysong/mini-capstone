31c067e6af615565f0e40f038b4b1f967e9aaf59
Richard Song <richardsong18@gmail.com>
2018-04-08 11:33:38 -0500

    Use JavaScript web request to get and display product data from backend

--- public/index.js
+++ public/index.js
@@ -1,11 +1,19 @@
+/* global axios */
+
 var productTemplate = document.querySelector("#product-card");
 var productContainer = document.querySelector(".row");
 
-productContainer.appendChild(productTemplate.content.cloneNode(true));
-productContainer.appendChild(productTemplate.content.cloneNode(true));
-productContainer.appendChild(productTemplate.content.cloneNode(true));
-productContainer.appendChild(productTemplate.content.cloneNode(true));
-productContainer.appendChild(productTemplate.content.cloneNode(true));
-productContainer.appendChild(productTemplate.content.cloneNode(true));
-productContainer.appendChild(productTemplate.content.cloneNode(true));
-productContainer.appendChild(productTemplate.content.cloneNode(true));
+axios.get("http://localhost:3000/v1/products").then(function(response) {
+  var products = response.data;
+  console.log(products);
+
+  products.forEach(function(product) {
+    var productClone = productTemplate.content.cloneNode(true);
+    productClone.querySelector(".card-title").innerText = product.name;
+    productClone.querySelector(".card-text").innerText = product.description;
+    productClone.querySelector(".list-group-item").innerText =
+      "price: $" + product.price;
+    productClone.querySelector(".card-img-top").src = product.images[0];
+    productContainer.appendChild(productClone);
+  });
+});
