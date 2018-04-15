8e418d39da77696e229d485b94933233cc02017a
Richard Song <richardsong18@gmail.com>
2018-04-15 10:46:43 -0500

    Add sign up component

--- public/index.js
+++ public/index.js
@@ -31,10 +31,44 @@ var SamplePage = {
   computed: {}
 };
 
+var SignupPage = {
+  template: "#signup-page",
+  data: function() {
+    return {
+      name: "",
+      email: "",
+      password: "",
+      passwordConfirmation: "",
+      errors: []
+    };
+  },
+  methods: {
+    submit: function() {
+      var params = {
+        name: this.name,
+        email: this.email,
+        password: this.password,
+        password_confirmation: this.passwordConfirmation
+      };
+      axios
+        .post("/v1/users", params)
+        .then(function(response) {
+          router.push("/login");
+        })
+        .catch(
+          function(error) {
+            this.errors = error.response.data.errors;
+          }.bind(this)
+        );
+    }
+  }
+};
+
 var router = new VueRouter({
   routes: [
     { path: "/", component: HomePage },
-    { path: "/sample", component: SamplePage }
+    { path: "/sample", component: SamplePage },
+    { path: "/signup", component: SignupPage }
   ],
   scrollBehavior: function(to, from, savedPosition) {
     return { x: 0, y: 0 };
