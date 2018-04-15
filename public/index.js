8b1e74ea0ec93be9aa1f1f6a989baf1a36821287
Richard Song <richardsong18@gmail.com>
2018-04-15 09:39:04 -0500

    Add sample page with route and links

--- public/index.js
+++ public/index.js
@@ -19,8 +19,23 @@ var HomePage = {
   computed: {}
 };
 
+var SamplePage = {
+  template: "#sample-page",
+  data: function() {
+    return {
+      message: "Welcome to Vue.js (sample page)!"
+    };
+  },
+  created: function() {},
+  methods: {},
+  computed: {}
+};
+
 var router = new VueRouter({
-  routes: [{ path: "/", component: HomePage }],
+  routes: [
+    { path: "/", component: HomePage },
+    { path: "/sample", component: SamplePage }
+  ],
   scrollBehavior: function(to, from, savedPosition) {
     return { x: 0, y: 0 };
   }
